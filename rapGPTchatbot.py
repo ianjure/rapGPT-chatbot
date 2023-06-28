c98764b39eaa74f2461f77cac464f1395270e7b2
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2023-06-28 11:46:18 +0800

    Update rapGPTchatbot.py

--- rapGPTchatbot.py
+++ rapGPTchatbot.py
@@ -79,8 +79,10 @@ chatbot.change_conversation(id)
 
 lottie_anim = load_lottiefile('robot_anim.json')
 
-col1, col2 = st.columns([80,50])
+col1, col2 = st.columns([80,30])
 with col1:
+    st.markdown('')
+     st.markdown('')
     st.title('RapGPT Chatbot')
 with col2:
     st_lottie(lottie_anim, loop = True, quality = 'high')
